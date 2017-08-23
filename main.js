280720ed1a8f78323588d3f5a898c18ed9155fd0
Ken Garcia <kenrinfeld.garcia@gmail.com>
2017-08-23 21:29:16 +0800

    Latest Update

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
+
   $.getJSON('test.json', function(data){
+
     for(var x in data){
       var make = data[x][0].make;
-      var make_list = "<div class='vehicle'>" + "<h2>"+ make +"</h2>" + "</div>";
-      $('.panel').append(make_list);
+      var image = data[x][0].images;
+
+      var vehicle = "<div class='vehicle col-lg-12 col-md-6'>";
+          vehicle += "<div class='image'><img src="+ image +" class='img-responsive'></div>";
+          vehicle += "<h1>" + make + "</h1>";
+          vehicle += '<button class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Show Details</button>';
+          vehicle += '<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">';
+          <div class="modal-dialog modal-lg"><div class="modal-content"><div class="img-container"></div><div class="modal_vehicle">test                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+          vehicle += "</div>";
+
+      $(".panel").append(vehicle);
+
+
     }
-  })
+  });
 
 
 });
