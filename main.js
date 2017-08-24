8a9d3a7090c67f5bb4b6ecbd3191abe8237ffcbd
Ken Garcia <kenrinfeld.garcia@gmail.com>
2017-08-24 18:37:44 +0800

    test

--- main.js
+++ main.js
@@ -11,12 +11,8 @@ $(document).ready(function(){
           vehicle += "<h1>" + make + "</h1>";
           vehicle += '<button class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Show Details</button>';
           vehicle += '<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">';
-          <div class="modal-dialog modal-lg"><div class="modal-content"><div class="img-container"></div><div class="modal_vehicle">test                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-          vehicle += "</div>";
+          vehicle += '<div class="modal-dialog modal-lg"><div class="modal-content"><div class="img-container"></div><div class="modal_vehicle">test</div></div></div></div></div>';
+          vehicle += '</div>';
 
       $(".panel").append(vehicle);
 
