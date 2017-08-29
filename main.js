cfdfbfe150f8092b5b1cf0cb79b3d057bdddf6b2
Ken Garcia <kenrinfeld.garcia@gmail.com>
2017-08-29 14:03:25 +0800

    Filter search added

--- main.js
+++ main.js
@@ -1,41 +1,60 @@
 $(document).ready(function(){
 
-  $.getJSON('test.json', function(data){
-    for(var x in data){
-      var make = data[x][0].make;
-      var image = data[x][0].images;
-      var equipment = data[x][0].equipment;
-      var model = data[x][0].model;
-      var trim = data[x][0].trim;
-      var year = data[x][0].year;
+    $.getJSON('test.json', function(data){
+      display(data);
+      $('.btn').click(function(){
+        var vehicle_make  = $(this).siblings().text();
+        var vehicle_image = $(this).siblings().html();
 
-      var vehicle = "<div class='vehicle col-lg-4 col-md-6 col-sm-12 col-xs-12'>";
-          vehicle += "<div class='image'><img src="+ image +" class='img-responsive'></div>";
-          vehicle += "<h1 class='make'>" + make + "</h1>";
-          vehicle += "<span class='equip'>"+ equipment +"</span>";
-          vehicle += "<p class='model'>" + "<b> Model : </b>" + model + "</p>";
-          vehicle += "<p class='trim'>" + "<b> Trim : </b>" + trim + "</p>";
-          vehicle += "<p class='year'>" + "<b> Year : </b>" + year + "</p>";
-          vehicle += "<button class='btn btn-primary' data-toggle='modal' data-target='#myModal'>Show Details</button>";
-          vehicle += "</div>";
+        $('.vehicle_make').append(vehicle_make);
+        $('.vehicle_image').append(vehicle_image);
+      });
 
-      $(".panel").append(vehicle);
+      $('.modal-footer button, .close').click(function(){
+        $('.vehicle_make').text('');
+        $('.vehicle_image img').detach();
+      });
+    });
 
+    function display(data){
+        for(var x in data){
+          var make = data[x][0].make;
+          var makeUppercase = make.toUpperCase();
+          var image = data[x][0].images;
+          var equipment = data[x][0].equipment;
+          var model = data[x][0].model;
+          var trim = data[x][0].trim;
+          var year = data[x][0].year;
+
+          var vehicle = "<div class='vehicle col-lg-4 col-md-6 col-sm-12 col-xs-12'>";
+              vehicle += "<div class='image'><img src="+ image +" class='img-responsive'></div>";
+              vehicle += "<h1 class='make'>" + makeUppercase + "</h1>";
+              vehicle += "<span class='equip'>"+ equipment +"</span>";
+              vehicle += "<p class='model'>" + "<b> Model : </b>" + model + "</p>";
+              vehicle += "<p class='trim'>" + "<b> Trim : </b>" + trim + "</p>";
+              vehicle += "<p class='year'>" + "<b> Year : </b>" + year + "</p>";
+              vehicle += "<button class='btn btn-primary' data-toggle='modal' data-target='#myModal'>Show Details</button>";
+              vehicle += "</div>";
+          $(".panel").append(vehicle);
+        }
     }
 
-    $('.btn').click(function(){
-      var vehicle_make  = $(this).siblings().text();
-      var vehicle_image = $(this).siblings().html();
+    $("#myInput").on("keyup", function(){
+      var input, filter, vehicle, make;
+      input   = $(this).val();
+      filter  = input.toUpperCase();
+      vehicle = $(".vehicle");
 
-      $('.vehicle_make').append(vehicle_make);
-      $('.vehicle_image').append(vehicle_image);
+      for(var x = 0; x < vehicle.length; x++){
+        make = vehicle[x];
+        var h1 = $(make).find("h1").text();
+        if(h1.indexOf(filter) > -1){
+          $(make).css("display","");
+        }else{
+          $(make).css("display","none");
+        }
+      }
 
     });
-    $('.modal-footer button, .close').click(function(){
-      $('.vehicle_make').text('');
-      $('.vehicle_image img').detach();
-    });
-
-  });
 
 });
